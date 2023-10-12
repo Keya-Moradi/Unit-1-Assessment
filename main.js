dd507d5af0ae272a72607c1ac4db7e983f355951
keya-moradi <keya.moradi@gmail.com>
2023-10-11 21:55:35 -0700

    feat: make progress #4

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-const tempForm = document.querySelector('#tempForm');
 
+const tempForm = document.querySelector('#tempForm');
 
 // grabbing buttons
 const increase = document.getElementById("increase");
@@ -7,16 +7,12 @@ const decrease = document.getElementById("decrease");
 
 // grabbing the number element
 const upperNumber = document.getElementById("upperNumber");
-
 const inputNumber = document.getElementById("inputNumber");
-
-// setting the initial number
 
 // adding event listeners to the buttons
 increase.addEventListener("click", function () {
     //number++;
     const number = parseInt(inputNumber.value) + 1;
-
     upperNumber.innerHTML = number;
 });
 
